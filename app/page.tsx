3e837303cc29822712618fa2f52b27be8a66595a
Nikhil <nikhil@gpu.net>
2025-08-22 14:41:45 +0530

    fix: some build issues

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ import VisitorCounter from '@/components/ui/VisitorCounter'
 export default function Home() {
 
   return (
-    <div className="w-screen h-screen " >
+    <div className="w-screen h-screen">
       <div className='overlay'></div>
         <video src="/bg.mp4" loop autoPlay muted playsInline className="h-[100%] w-[100%] object-cover"></video>
-        <div className='content flex  ' >
+        <div className='content flex' >
             <div className='w-[640px] m-auto border-[0.05px] border-opacity-5 border-white h-[auto]'>
               <div className='px-4 pt-2 absolute text-white md:hidden' >
               <Sheet>
